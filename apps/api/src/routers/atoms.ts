9c154ef1fd0586e9958d95731694544a1935fa64
sqmasep <leblond.romain@orange.fr>
2023-10-18 00:18:12 +0200

    [FEAT] Added some routes

--- apps/api/src/routers/atoms.ts
+++ apps/api/src/routers/atoms.ts
@@ -6,7 +6,7 @@ const atomsRouter = express.Router();
 
 atomsRouter.get("/", async (req, res) => {
   const data = await db.atom.findMany();
-  res.status(200).send(data);
+  return res.status(200).send(data);
 });
 
 atomsRouter.get("/byAtomicNumber/:atomicNumber", async (req, res) => {
@@ -14,16 +14,20 @@ atomsRouter.get("/byAtomicNumber/:atomicNumber", async (req, res) => {
     const sanitizedAtomicNumber = z
       .number()
       .positive()
+      .min(1)
       .parse(req.params.atomicNumber);
 
     const data = await db.atom.findUniqueOrThrow({
       where: { atomicNumber: sanitizedAtomicNumber },
     });
+
+    return res.status(200).send(data);
   } catch (error) {}
 });
 
 atomsRouter.get("/byId/:id", async (req, res) => {
   try {
+    // WARN [VALIDATION] this may require more precision, like cuid or uuid or the correct id type
     const sanitizedId = z.string().parse(req.params.id);
 
     const data = await db.atom.findUniqueOrThrow({
@@ -31,10 +35,78 @@ atomsRouter.get("/byId/:id", async (req, res) => {
         id: sanitizedId,
       },
     });
+
     return res.status(200).send(data);
   } catch (error) {
     return res.status(500).send({ message: error });
   }
 });
+
+atomsRouter.get("/byDiscoveryYear/before/:date", async (req, res) => {
+  try {
+    const sanitizedDate = z
+      .number()
+      .int()
+      .positive()
+      .parse(Number(req.params.date));
+
+    const data = await db.atom.findMany({
+      where: { discovery: { is: { year: { lt: sanitizedDate } } } },
+    });
+
+    return res.status(200).send(data);
+  } catch (error) {}
+});
+
+atomsRouter.get("/byDiscoveryYear/after/:date", async (req, res) => {
+  try {
+    const sanitizedDate = z
+      .number()
+      .int()
+      .positive()
+      .parse(Number(req.params.date));
+
+    const data = await db.atom.findMany({
+      where: { discovery: { is: { year: { gt: sanitizedDate } } } },
+    });
+
+    return res.status(200).send(data);
+  } catch (error) {}
+});
+
+atomsRouter.get("/byPhaseAtSTP/:phase", async (req, res) => {
+  try {
+    const sanitizedPhase = z
+      .enum(["gas", "liquid", "solid", "plasma"])
+      .parse(req.params.phase);
+
+    const data = await db.atom.findMany({
+      where: {
+        phaseAtSTP: sanitizedPhase,
+      },
+    });
+
+    return res.status(200).send(data);
+  } catch (error) {}
+});
+
+atomsRouter.get("/metals", async (req, res) => {
+  try {
+    const data = await db.atom.findMany({
+      where: {
+        family: {
+          is: {
+            isMetal: true,
+          },
+        },
+      },
+    });
+
+    return res.status(200).send(data);
+  } catch (error) {}
+});
+
+atomsRouter.get("/byBlock/:block", async (req, res) => {});
+atomsRouter.get("/byBlock/:block", async (req, res) => {});
 
 export default atomsRouter;
